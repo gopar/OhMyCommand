f0747f43804c013ab34e4c1f709543b3cc3ad326
Daniel Gopar <gopardaniel@yahoo.com>
2016-04-03 18:00:01 -0700

    Refactored tests and added tests for Commands

--- apps/commands/tests/test_api.py
+++ apps/commands/tests/test_api.py
@@ -4,9 +4,19 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 
-from population_script import populate, add_user, add_command
+from population_script import add_user, add_command
 
+from apps.commands.models import Command
 User = get_user_model()
+
+
+def create_client(data, url, get=True, kwargs=None):
+    client = APIClient()
+    client.login(username=data['username'],
+                 password=data['password'])
+    if get:
+        return client.get(url, kwargs=kwargs or None)
+    return client.post(url, data=kwargs or None)
 
 
 class TestUserAPI(APITestCase):
@@ -16,22 +26,15 @@ class TestUserAPI(APITestCase):
                           'email': 'email@email.com'}
         add_user(**self.user_data)
 
-    def create_client(self, url, get=True, kwargs=None):
-        client = APIClient()
-        client.login(username=self.user_data['username'],
-                     password=self.user_data['password'])
-        if get:
-            return client.get(url, kwargs=kwargs or None)
-        return client.post(url, data=kwargs or None)
-
     def test_GET_user(self):
-        response = self.create_client(reverse('user-list'))
+        response = create_client(self.user_data, reverse('user-list'))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
 
     def test_GET_user_detail(self):
-        response = self.create_client(
+        response = create_client(
+            self.user_data,
             reverse('user-detail', kwargs={'pk': User.objects.first().pk}))
 
         updated_dict = {'pk': User.objects.first().pk}
@@ -43,7 +46,8 @@ class TestUserAPI(APITestCase):
 
     def test_POST_user_list(self):
         temp = {'username': 'use', 'password': 'pass', 'email': 'e@email.com'}
-        response = self.create_client(
+        response = create_client(
+            self.user_data,
             reverse('user-list'), get=False, kwargs=temp)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -66,3 +70,53 @@ class TestUserAPI(APITestCase):
         self.assertNotEqual(user.first_name, response.data['first_name'])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(temp['first_name'], response.data['first_name'])
+
+
+class TestCommandAPI(APITestCase):
+    def setUp(self):
+        self.data = {'username': 'username', 'password': 'password',
+                     'first_name': 'first', 'last_name': 'last',
+                     'email': 'email@email.com'}
+        user = add_user(**self.data)
+        self.c_data = {'command': 'ls -al', 'user': user,
+                       'note': '', 'os': '', 'version': ''}
+        self.command = add_command(**self.c_data)
+
+    def test_GET_command_list(self):
+        response = create_client(self.data, reverse('commands-list'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), Command.objects.count())
+
+    def test_GET_command_detail(self):
+        response = create_client(
+            self.data,
+            reverse('commands-detail', kwargs={'pk': self.command.pk}))
+        temp = {'id': self.command.pk}
+        temp.update(self.c_data)
+        temp['user'] = temp['user'].pk
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, temp)
+
+    def test_POST_command_list(self):
+        data = {'command': 'command', 'os': 'L', 'note': 'note',
+                'version': '1', 'user': self.c_data['user'].pk}
+        response = create_client(self.data, reverse('commands-list'),
+                                 get=False, kwargs=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertDictContainsSubset(data, response.data)
+
+    def test_POST_command_detail(self):
+        data = {'os': 'W'}
+        client = APIClient()
+        client.login(username=self.data['username'],
+                     password=self.data['password'])
+        response = client.patch(
+            reverse('commands-detail', kwargs={'pk': self.command.pk}),
+            data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['os'], data['os'])
+        self.assertNotEqual(self.command.os, data['os'])
